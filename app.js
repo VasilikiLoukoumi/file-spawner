f8e5e838332ecf71e6176c31bf355b9da2486e2e
Vasiliki Loukoumi <vloukoumi@binarylogic.local>
2018-08-01 16:01:42 +0300

    Added functionality so that file is placed wherever the user wants.

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 var prompt = require('prompt');
 var fs = require('fs');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
 prompt.start();
 
-prompt.get(['user', 'filename', 'file_extension', 'description', 'content'], function (err, result) {
-    console.log('Input taken by user ' + result.user + '.');
+prompt.get(['user', 'filename', 'file_extension', 'description', 'content', 'directory'], function (err, result) {
+    
     result.filename += "." + result.file_extension;
+    result.directory += "/" + result.filename;
     fs.appendFile(result.filename, result.content, function (err) {
         if (err) {
             throw err;
         }
         else {
-            console.log("File " + result.filename + " created by user " + result.user + " at " + new Date().toString() + ".");
+            console.log("*");
+            console.log("A file named " + result.filename + ",\nwas created by user " + result.user + ",\nat " + new Date().toString() + ".");
+            console.log("DETAILS:");
             console.log("Description:");
-            console.log(result.description);
+            console.log(result.description);            
+            eventEmitter.on('placeInDirectory', moveFile);
+            eventEmitter.emit('placeInDirectory');
         }
     });
-   
+
+   var moveFile = function() {
+        fs.rename(result.filename, result.directory, function (err) {
+            if (err) {
+                throw err;
+            }
+            else {
+                console.log('Location:\n' + result.directory.split('/')[0]);
+                console.log("*");
+            }
+
+        });
+    }
+
 });
 
 
